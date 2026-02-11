d70847318b8d8324619e919e88f53ae02ad425b0
Ervin-me <ervin398@gmail.com>
2026-02-11 09:23:24 +0100

    added repository

--- src/main/java/se/iths/connie/movierater/repository/ReviewRepository.java
+++ src/main/java/se/iths/connie/movierater/repository/ReviewRepository.java
@@ -2,7 +2,6 @@ package se.iths.connie.movierater.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import se.iths.connie.movierater.model.Director;
 import se.iths.connie.movierater.model.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
